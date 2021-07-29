551717d3c7f54e37509532a4e7cc08c851fdbe31
cz6 <chenzb961@163.com>
2021-07-29 21:07:55 +0800

    项目优化

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,36 +3,38 @@
     <el-container>
       <!-- 顶部 -->
       <el-header height="65px">
-        <el-dropdown size="small">
-          <el-button type="primary">
-            <el-avatar :src="user.avatar_url" size="medium"> </el-avatar>
-          </el-button>
-          <template #dropdown>
-            <el-dropdown-menu>
-              <el-dropdown-item disabled>
-                用户名：{{ user.name }}
-              </el-dropdown-item>
-              <el-dropdown-item disabled>
-                账号：{{ user.email }}
-              </el-dropdown-item>
-              <el-dropdown-item @click="logout" icon="el-icon-switch-button">
-                注销
-              </el-dropdown-item>
-            </el-dropdown-menu>
-          </template>
-        </el-dropdown>
+        <span class="log">企鹅后台管理系统</span>
+        <span class="el-icon-s-operation" @click="swith"></span>
+        <div class="right">
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              <span> 账号：{{ user.email }}</span>
+              <i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item>个人中心</el-dropdown-item>
+                <el-dropdown-item @click="logout"> 退出登录 </el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+          <img :src="user.avatar_url" alt="" />
+        </div>
       </el-header>
       <el-container>
         <!-- 左侧导航
         router开启路由模式绑定导航index为跳转path后去配置路由文件
         default-active绑定活跃路由的路径后刷新页面会保持高亮
         uniqueOpened只保持一个子菜单的展开
+        :动态绑定可以写三元运算符
          -->
-        <el-aside width="200px">
+        <el-aside :width="!isCollapse ? '200px' : '65px'">
           <el-menu
+            :collapse="isCollapse"
             :uniqueOpened="true"
-            background-color="#d3dce6"
-            text-color="#333"
+            :collapse-transition="false"
+            background-color="#424242"
+            text-color="#fff"
             active-text-color="#66b1ff"
             router
             :default-active="$route.path"
@@ -85,8 +87,9 @@ export default {
         "el-icon-s-promotion",
         "el-icon-s-order",
         "el-icon-picture",
-        "el-icon-s-operation",
+        "el-icon-s-tools",
       ],
+      isCollapse: false,
     };
   },
   computed: {},
@@ -102,6 +105,9 @@ export default {
       });
       //跳转到login
       this.$router.push("/login");
+    },
+    swith() {
+      this.isCollapse = !this.isCollapse;
     },
   },
   mounted() {
@@ -119,15 +125,14 @@ export default {
 
 <style scoped>
 .el-header {
-  background-color: #b3c0d1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-  height: 65px;
+  position: relative;
+  background-color: #212121;
+  color: #fff;
+  line-height: 65px;
 }
 .el-aside {
-  background-color: #d3dce6;
-  color: #333;
+  background-color: #424242;
+  color: #fff;
 }
 .el-main {
   background-color: #e9eef3;
@@ -139,5 +144,29 @@ export default {
 /* 去除左边导航的边框 */
 .el-menu {
   border-right: 0;
+}
+.log {
+  display: inline-block;
+  width: 180px;
+}
+.el-icon-s-operation {
+  font-size: 1.5em;
+}
+.right {
+  display: inline-block;
+  position: absolute;
+  right: 20px;
+  padding: 10px 0;
+  box-sizing: border-box;
+  height: 65px;
+}
+.right img {
+  margin: 0 20px;
+  height: 45px;
+  width: 45px;
+}
+.el-dropdown-link {
+  color: #fff;
+  font-size: 1.2em;
 }
 </style>
